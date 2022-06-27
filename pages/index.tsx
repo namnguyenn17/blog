3f1054373a638c09c58f85706bb78541d7a47f91
Nam Nguyen <ducnam1719@gmail.com>
2022-06-28 05:28:48 +0700

    Some customize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,41 @@
-import Link from 'next/link'
+import Link from "next/link"
 
 const NOTION_BLOG_ID =
-  process.env.NOTION_BLOG_ID || 'e91ef7de6e49496b99f695756ae20211'
+    process.env.NOTION_BLOG_ID || "d5783b10f6414a068e03259a76a4e0e2"
 
 export type Post = { id: string; slug: string; title: string; date: string }
 
 export const getAllPosts = async (): Promise<Post[]> => {
-  return await fetch(
-    `https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`
-  ).then((res) => res.json())
+    return await fetch(
+        `https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`
+    ).then((res) => res.json())
 }
 
 export async function getStaticProps() {
-  const posts = await getAllPosts()
-  return {
-    props: {
-      posts,
-    },
-  }
+    const posts = await getAllPosts()
+    return {
+        props: {
+            posts,
+        },
+    }
 }
 
 function HomePage({ posts }: { posts: Post[] }) {
-  return (
-    <div className="content">
-      <h1>Posts</h1>
-      <div>
-        {posts.map((post) => (
-          <Link href="/blog/[slug]" as={`/blog/${post.slug}`}>
-            <a>
-              <b>{post.title}</b>
-              <div className="sub">posted on {post.date}</div>
-            </a>
-          </Link>
-        ))}
-      </div>
-    </div>
-  )
+    return (
+        <div className="content">
+            <h1>Posts</h1>
+            <div>
+                {posts.map((post) => (
+                    <Link href="/blog/[slug]" as={`/blog/${post.slug}`}>
+                        <a>
+                            <b>{post.title}</b>
+                            <div className="sub">posted on {post.date}</div>
+                        </a>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
