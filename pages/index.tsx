7adc6724012a2fac19ea9900dfded7a7eee4b315
Nam Nguyen <ducnam1719@gmail.com>
2022-06-28 05:19:14 +0700

    some customize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 const NOTION_BLOG_ID =
-  process.env.NOTION_BLOG_ID || "1099525da7e5405c961706de56622ccd";
+  process.env.NOTION_BLOG_ID || 'e91ef7de6e49496b99f695756ae20211'
 
-export type Post = { id: string; slug: string; title: string; date: string };
+export type Post = { id: string; slug: string; title: string; date: string }
 
 export const getAllPosts = async (): Promise<Post[]> => {
   return await fetch(
     `https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`
-  ).then((res) => res.json());
-};
+  ).then((res) => res.json())
+}
 
 export async function getStaticProps() {
-  const posts = await getAllPosts();
+  const posts = await getAllPosts()
   return {
     props: {
       posts,
     },
-  };
+  }
 }
 
 function HomePage({ posts }: { posts: Post[] }) {
@@ -35,7 +35,7 @@ function HomePage({ posts }: { posts: Post[] }) {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
