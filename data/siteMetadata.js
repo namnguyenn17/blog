f4bf4e5b139b654aec000d73d4941e2bcc194270
Nam Nguyen <ducnam1719@gmail.com>
2022-07-10 15:48:08 +0700

    add site metadata

---
+++ data/siteMetadata.js
@@ -0,0 +1,20 @@
+const siteMetadata = {
+  title: "NamNguyen's Portfolio",
+  author: 'Nam Nguyen',
+  headerTitle: "NamNguyen's Portfolio",
+  description:
+    "I'm Nam. A boy dreams to create value for others via software products. Welcome to my corner of the internet. I'm glad you're here!",
+  language: 'en-us',
+  siteUrl: 'http://www.namnguyen.blog',
+  siteRepo: 'https://github.com/nam-nguyen17/blog',
+  email: 'mailto:ducnam1719@gmail.com',
+  github: 'https://github.com/nam-nguyen17',
+  twitter: 'https://twitter.com/NamNguyen579',
+  linkedin: 'https://www.linkedin.com/in/namnguyen17/',
+  locale: 'vi-VN',
+  analytics: {
+    plausibleDataDomain: 'namnguyen.blog',
+  },
+}
+
+module.exports = siteMetadata
