43ccb4980944e6075181e197e61076559831634c
Nam Nguyen <ducnam1719@gmail.com>
2023-12-24 21:25:00 +0700

    updating Reaction component of visual difference & has read the blog

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   images: {
     domains: [
-      's3.us-west-2.amazonaws.com',
-      'via.placeholder.com',
-      'images.unsplash.com'
+      's3.us-west-2.amazonaws.com', // Images coming from Notion
+      'via.placeholder.com', // for articles that do not have a cover image
+      'images.unsplash.com', 
+      'dwgyu36up6iuz.cloudfront.net',
+      'cdn.hashnode.com',
+      'res.craft.do',
+      'prod-files-secure.s3.us-west-2.amazonaws.com', // Add this line for the new hostname
     ]
   }
 };
