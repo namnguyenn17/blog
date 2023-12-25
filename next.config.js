1bc8560edf5a2938403e235a62d6bad833dd1e6f
Nam Nguyen <ducnam1719@gmail.com>
2023-12-25 09:26:27 +0700

    responsive newsletter form on mobile & tablet

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ module.exports = {
     domains: [
       's3.us-west-2.amazonaws.com', // Images coming from Notion
       'via.placeholder.com', // for articles that do not have a cover image
-      'images.unsplash.com', 
+      'images.unsplash.com',
       'dwgyu36up6iuz.cloudfront.net',
       'cdn.hashnode.com',
       'res.craft.do',
-      'prod-files-secure.s3.us-west-2.amazonaws.com', // Add this line for the new hostname
+      'prod-files-secure.s3.us-west-2.amazonaws.com' // Add this line for the new hostname
     ]
   }
 };
